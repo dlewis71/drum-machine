4dbb978e191d4ba02bdecc9aadb48361bba2944c
dlewis71 <dlewis@perseverenow.org>
2025-06-30 15:20:25 -0500

    my Home work

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+let count = 1;
 
 // This function is called every 600ms
 function update() {
+    if(count === 4) {
+        tock.play();
+    } else {
+        tick.play();
+    }
+count = count % 4 + 1;
+}
 
-    // Play the 'tick' sound
-    tick.play();
+function updatetock() {
+    tock.play();
 }
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+    setTimeout(setupUpdate, 600);{
+        if (count === 4) {
+            tock.play();
+        } else {
+            tick.play();
+        }
+        count = count % 4 + 1;
+    }
+    function updatetock() {
+        tock.play();
+    }
 }
 
 // Call setupUpdate() once after 300ms
